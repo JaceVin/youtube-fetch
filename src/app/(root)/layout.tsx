8a2abb7bbd4fe05fa03c4cca8408c7c5ac43fcc2
JaceVin <denzelhdez09@gmail.com>
2024-03-30 19:27:28 -0400

    Configured metadata.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./scss/globals.scss";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './scss/globals.scss';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: 'JaceVin Fetcher',
+  description: 'Collection of API fetchers.',
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
   );
